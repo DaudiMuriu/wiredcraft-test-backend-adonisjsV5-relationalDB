60aa69a71750b53be26a1505d451dbfb51dddb69
MuriuDaudi <daudi@sendyit.com>
2023-10-28 20:46:51 +0300

    Change routing for users
    Signed-off-by: MuriuDaudi <daudi@sendyit.com>

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -1,13 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('', 'UsersController.index') //Lists users
-
-  Route.post('', 'UsersController.store') //Store user
-
-  Route.group(() => {
-    Route.patch('', 'UsersController.update') //Update user
-
-    Route.delete('', 'UsersController.destroy') //Delete user
-  }).prefix(':userID')
-}).prefix('/api/v1/users')
+  Route.resource('users', 'UsersController').except(['edit', 'create'])
+}).prefix('/api/v1')
